d637cd42582e2605f03b6625edaa0349cef3720e
Prabhjot <prabadelic@gmail.com>
2024-11-05 21:35:28 -0500

    updating env and cors

--- welovemovies-back-end/src/reviews/reviews.router.js
+++ welovemovies-back-end/src/reviews/reviews.router.js
@@ -1,7 +1,6 @@
 const router = require("express").Router({ mergeParams: true });
 const controller = require("./reviews.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
-const cors = require("cors");
     
 router
     .route("/:reviewId")
